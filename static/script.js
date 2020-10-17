e700feeb73c79d5640a642f4f56e5e38f87b0d3c
Ogecho Onuoha <oge.blessing@live.com>
2020-10-17 11:01:21 +0100

    add image download functionality

--- static/script.js
+++ static/script.js
@@ -47,11 +47,6 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: 'eu-west-2:827977d6-6704-4ebf-8854-54526f23cb0b',
 });
 
-var s3 = new AWS.S3({
-  apiVersion: "2006-03-01",
-  params: { Bucket: bucketName }
-});
-
 function addPhoto() {
     s3url = null
     var files = document.getElementById("upload").files;
@@ -60,45 +55,47 @@ function addPhoto() {
     }
     var file = files[0];
     var fileName = Date.now().toString()+ '_' + file.name;
-    var albumPhotosKey = encodeURIComponent(uploadfoldername) + "/";
+    var uploadphotoKey = encodeURIComponent(uploadfoldername) + "/" + fileName;
+    var downloadphotoKey = encodeURIComponent(downloadfoldername) + "/" + fileName;
     
-    var photoKey = albumPhotosKey + fileName;
   
     // Use S3 ManagedUpload class as it supports multipart uploads
     var upload = new AWS.S3.ManagedUpload({
       params: {
         Bucket: bucketName,
-        Key: photoKey,
+        Key: uploadphotoKey,
         Body: file,
         ACL: "public-read"
       }
     });
+
+    //get ready for download
+    var download = new AWS.S3({
+      apiVersion: "2006-03-01"
+    });
+    var downloadparams = { Bucket: detectionbucket ,
+      Kcd pbw ey: downloadphotoKey
+    };
   
     var promise = upload.promise();
-  
     promise.then(
       function(data) {
         alert("Successfully uploaded photo.");
         
         s3url = 'https://'+bucketName+'.s3.'+ bucketRegion+'.amazonaws.com/'+photoKey;
+        //console.log(s3url);
         files = []
         clearFileInput(document.getElementById("upload"));
-        console.log(s3url);
         document.getElementById("upload-label").innerHTML = 'Choose image';
-        //const { ContentType } = await s3.getObject(params).promise();
-        /*$.ajax({
-          method: 'POST',
-          url: 'https://18.133.189.143:5000/detect',
-          data: {"url" : s3url},
-          contentType: false,
-          processData: false,
-      }).done(function(data, textStatus, jqXHR){
-        console.log('results!!!');
-        console.log(data);
-          //$('#result').text(data);
-      }).fail(function(data){
-          alert('error!');
-      }); */
+        upload_response = await download.getObject(downloadparams).promise();
+        upload_response.then(
+          function(data){
+            console.log(data)
+          },
+          function(down_err){
+            console.log(down_err)
+          }
+        );
 
       },
       function(err) {
